44a199ca310b64bb4e14047b6bc9de808774c399
petivip <petivip@gmail.com>
2019-11-15 11:37:32 -0500

    intial

---
+++ src/main/java/com/example/demo/BootStrap101Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootStrap101Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootStrap101Application.class, args);
+    }
+
+}
